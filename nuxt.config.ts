cdc89f2e25b42e6a068bb42b7baf72e2f8fa4d11
Carson Stone <carson@landis.com>
2022-11-12 22:04:39 -0500

    add open ai org id env variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   css: ["bootstrap/dist/css/bootstrap.min.css"],
   runtimeConfig: {
+    OPEN_AI_ORG_ID: process.env.OPEN_AI_ORG_ID, // only exposed server-side
     OPEN_AI_API_SECRET_KEY: process.env.OPEN_AI_API_SECRET_KEY // only exposed server-side
   }
 })
