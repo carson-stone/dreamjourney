0adefb3919d0204fd63f089ea5acadce6eaa42ef
Carson Stone <carson@landis.com>
2022-11-12 21:22:11 -0500

    expose open ai api key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["bootstrap/dist/css/bootstrap.min.css"],
+  runtimeConfig: {
+    OPEN_AI_API_SECRET_KEY: process.env.OPEN_AI_API_SECRET_KEY // only exposed server-side
+  }
 })
